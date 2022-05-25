758e967a72b62a26a50edbfdad791e82c1fd62a9
Farid Attamimi <tamimi2021farid@gmail.com>
2022-05-25 11:53:06 +0200

    practice

---
+++ index.js
@@ -0,0 +1,29 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import App from "./App"
+
+ReactDOM.render(<App />, document.getElementById("root"))
+
+
+
+
+
+
+/*import React from "react"
+import ReactDOM from "react-dom"
+
+function App() {
+    const nums = [1, 2, 3, 4, 5, 6, 7, 8]
+    const result = nums.map(function(num) {
+        return num * 2
+    })
+    console.log(result);
+    return (
+        <h1>Test 1</h1>
+    )
+}
+
+
+ReactDOM.render(<App />, document.getElementById("root"))
+*/
